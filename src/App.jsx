4da330940381a69046ad12439c20a92ecdbb8d2b
Eduardo Prudencio <eduardoprudencio@Eduardos-MacBook-Pro.local>
2021-04-18 20:57:43 -0300

    Implementando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { Counter } from "./componentes/Counter";
 import { RepositoryList } from "./Componentes/RepositoryList";
 
 export function App(){
-    return <RepositoryList/>    
+    return <>
+    <RepositoryList/>
+    <Counter/>
+        </>
 }
