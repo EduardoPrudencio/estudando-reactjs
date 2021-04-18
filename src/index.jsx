68a37cca8182157fe9e45b778a97e126991fe2a1
Eduardo Prudencio <eduardoprudencio@Eduardos-MacBook-Pro.local>
2021-04-17 22:35:45 -0300

    Comletando a configuracao de ambiente

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,6 @@
-import React from 'react';
+import { render } from 'react-dom';
+import './styles/global.css';
 import { App } from './App';
+
+render(<App />, document.getElementById('root'))
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    devServer: {
+        contentBase: path.resolve(__dirname,'public')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,'public','index.html')
+        })
+    ],
     entry: path.resolve(__dirname,'src','index.jsx'),
 
     output: {
@@ -15,7 +28,13 @@ module.exports = {
             { 
                 test: /\.jsx$/, 
                 loader: 'babel-loader', 
-                exclude: /node_modules/ }
+                exclude: /node_modules/ 
+            },
+            { 
+                test: /\.css$/, 
+                exclude: /node_modules/,
+                use: ['style-loader','css-loader']
+            }
         ]
     }
 }
